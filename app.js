a487f7b78463bf1e2e89175049cecebbd86292a7
Alexandra <anance5@murraystate.edu>
2020-10-02 19:42:51 -0500

    installed inquirer

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
 
 
 
 
 
-  /* const printProfileData = profileDataArr => {
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+/* const printProfileData = profileDataArr => {
     // This...
     for (let i = 0; i < profileDataArr.length; i += 1) {
         console.log(profileDataArr[i]);
@@ -39,3 +46,4 @@ fs.writeFile('./index.html', generatePage(name, github), err => {
 };
   
 printProfileData(profileDataArgs); */
+
